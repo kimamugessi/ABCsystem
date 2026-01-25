d3122567014a71946927123ca40fc308d78fc260
jiyeah <kimamugessi@gmail.com>
2026-01-26 01:29:52 +0900

    찐 변경 완

--- ABCsystem/RunForm.Designer.cs
+++ ABCsystem/RunForm.Designer.cs
@@ -42,11 +42,13 @@
             // 
             // btnGrab
             // 
-            this.btnGrab.Font = new System.Drawing.Font("궁서", 22.25455F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnGrab.Font = new System.Drawing.Font("한컴산뜻돋움", 20.29091F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnGrab.ForeColor = System.Drawing.Color.Black;
+            this.btnGrab.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
             this.btnGrab.Location = new System.Drawing.Point(57, 22);
             this.btnGrab.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnGrab.Name = "btnGrab";
-            this.btnGrab.Size = new System.Drawing.Size(56, 70);
+            this.btnGrab.Size = new System.Drawing.Size(70, 70);
             this.btnGrab.TabIndex = 0;
             this.btnGrab.Text = "📸";
             this.btnGrab.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -55,12 +57,13 @@
             // 
             // btnStart
             // 
-            this.btnStart.Font = new System.Drawing.Font("궁서", 22.25455F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.btnStart.ForeColor = System.Drawing.Color.Green;
-            this.btnStart.Location = new System.Drawing.Point(54, 106);
+            this.btnStart.Font = new System.Drawing.Font("한컴산뜻돋움", 20.29091F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnStart.ForeColor = System.Drawing.Color.Black;
+            this.btnStart.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnStart.Location = new System.Drawing.Point(57, 106);
             this.btnStart.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(56, 70);
+            this.btnStart.Size = new System.Drawing.Size(70, 70);
             this.btnStart.TabIndex = 1;
             this.btnStart.Text = "▶️";
             this.btnStart.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -69,11 +72,13 @@
             // 
             // btnLive
             // 
-            this.btnLive.Font = new System.Drawing.Font("궁서", 22.25455F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.btnLive.Location = new System.Drawing.Point(124, 22);
+            this.btnLive.Font = new System.Drawing.Font("한컴산뜻돋움", 20.29091F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnLive.ForeColor = System.Drawing.Color.Black;
+            this.btnLive.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnLive.Location = new System.Drawing.Point(135, 22);
             this.btnLive.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnLive.Name = "btnLive";
-            this.btnLive.Size = new System.Drawing.Size(56, 70);
+            this.btnLive.Size = new System.Drawing.Size(70, 70);
             this.btnLive.TabIndex = 2;
             this.btnLive.Text = "🎥";
             this.btnLive.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -82,12 +87,13 @@
             // 
             // btmStop
             // 
-            this.btmStop.Font = new System.Drawing.Font("궁서", 22.25455F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.btmStop.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.btmStop.Location = new System.Drawing.Point(125, 106);
+            this.btmStop.Font = new System.Drawing.Font("한컴산뜻돋움", 20.29091F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btmStop.ForeColor = System.Drawing.Color.Black;
+            this.btmStop.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btmStop.Location = new System.Drawing.Point(135, 106);
             this.btmStop.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btmStop.Name = "btmStop";
-            this.btmStop.Size = new System.Drawing.Size(56, 70);
+            this.btmStop.Size = new System.Drawing.Size(70, 70);
             this.btmStop.TabIndex = 1;
             this.btmStop.Text = "⏹️";
             this.btmStop.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -99,7 +105,7 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.SystemColors.Window;
             this.label1.Font = new System.Drawing.Font("한컴산뜻돋움", 9.163635F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.Location = new System.Drawing.Point(69, 66);
+            this.label1.Location = new System.Drawing.Point(75, 68);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(34, 18);
@@ -111,7 +117,7 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.SystemColors.Window;
             this.label2.Font = new System.Drawing.Font("한컴산뜻돋움", 9.163635F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label2.Location = new System.Drawing.Point(134, 66);
+            this.label2.Location = new System.Drawing.Point(153, 68);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 18);
@@ -123,7 +129,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("한컴산뜻돋움", 9.818182F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label3.Location = new System.Drawing.Point(12, 133);
+            this.label3.Location = new System.Drawing.Point(15, 133);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(37, 19);
@@ -136,7 +142,7 @@
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.SystemColors.Window;
             this.label4.Font = new System.Drawing.Font("한컴산뜻돋움", 9.163635F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label4.Location = new System.Drawing.Point(66, 150);
+            this.label4.Location = new System.Drawing.Point(75, 152);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(34, 18);
@@ -149,7 +155,7 @@
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.SystemColors.Window;
             this.label5.Font = new System.Drawing.Font("한컴산뜻돋움", 9.163635F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label5.Location = new System.Drawing.Point(137, 150);
+            this.label5.Location = new System.Drawing.Point(153, 152);
             this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(34, 18);
