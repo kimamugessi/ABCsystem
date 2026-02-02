38bc201c00569f60a94b1d3a36c3a88581abb381
JIYE <kimamugessi@gmail.com>
2026-02-02 12:29:45 +0900

    아이콘 변경

--- ABCsystem/CameraForm.Designer.cs
+++ ABCsystem/CameraForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CameraForm));
             this.mainViewToolbar = new ABCsystem.UIControl.MainViewToolbar();
             this.imageViewer = new ABCsystem.UIControl.ImageViewCtrl();
             this.SuspendLayout();
@@ -35,8 +36,9 @@
             // mainViewToolbar
             // 
             this.mainViewToolbar.Dock = System.Windows.Forms.DockStyle.Right;
+            this.mainViewToolbar.Font = new System.Drawing.Font("한컴산뜻돋움", 9.163636F);
             this.mainViewToolbar.Location = new System.Drawing.Point(605, 0);
-            this.mainViewToolbar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.mainViewToolbar.Margin = new System.Windows.Forms.Padding(4);
             this.mainViewToolbar.Name = "mainViewToolbar";
             this.mainViewToolbar.Size = new System.Drawing.Size(73, 444);
             this.mainViewToolbar.TabIndex = 1;
@@ -44,33 +46,33 @@
             // imageViewer
             // 
             this.imageViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageViewer.Font = new System.Drawing.Font("한컴산뜻돋움", 9.163636F);
             this.imageViewer.Location = new System.Drawing.Point(0, 0);
             this.imageViewer.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.imageViewer.Name = "imageViewer";
             this.imageViewer.Size = new System.Drawing.Size(678, 444);
             this.imageViewer.TabIndex = 0;
             this.imageViewer.WorkingState = "";
-           // this.imageViewer.Load += new System.EventHandler(this.imageViewer_Load);
             // 
             // CameraForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(678, 444);
             this.Controls.Add(this.mainViewToolbar);
             this.Controls.Add(this.imageViewer);
             this.Font = new System.Drawing.Font("한컴산뜻돋움", 9.163635F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.Name = "CameraForm";
-            this.Text = "CameraForm";
+            this.Text = "Camera";
             this.Resize += new System.EventHandler(this.CameraForm_Resize);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private UIControl.ImageViewCtrl imageViewer;
         private UIControl.MainViewToolbar mainViewToolbar;
+        private UIControl.ImageViewCtrl imageViewer;
     }
 }
